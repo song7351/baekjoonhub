b3b3bf8424eee0f07162e42bef861409cdd8837b
song7351 <51012537+song7351@users.noreply.github.com>
2022-09-29 16:54:23 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 960 ms, Memory: 125424 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,14 +1,17 @@
 import sys
-
-
-test_case = int(input())
-
-num_list = []
-for i in range(test_case):
-    a = int(sys.stdin.readline())
-    num_list.append(a)
-
-num_list.sort()
-
-for i in num_list:
-    print(i)
+N = int(sys.stdin.readline())
+num_list = [False] * 2000001
+for i in range(N):
+    num_list[int(sys.stdin.readline())] = True
+print('\n'.join([str(num) for num in range(-1000000 , 1000001) if num_list[num] == True]))
+'''
+<참고용> 
+최초에 해당 범위 리스트 False를 만들고 받는 값들을 전부받고 True인 값들만
+출력한다면? 이게 왜 더 빠르지...?
+import sys
+N = int(sys.stdin.readline())
+num_list = [False] * 2000001
+for i in range(N):
+    num_list[int(sys.stdin.readline())] = True
+print('\n'.join([str(num) for num in range(-1000000 , 1000001) if num_list[num] == True]))
+'''

6a674f5abe393ee86e3725fc3330b932125d1f2e
song7351 <51012537+song7351@users.noreply.github.com>
2022-10-05 15:02:29 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -5,49 +5,34 @@
 1은 벽
 3은 파이프
 
-dfs방식 : 63% 시간초과
+dfs방식 : 63% 시간초과    (pypy는 통과)
+dp 구현
 """
-def search(i,j,d):
-    global ans
-    if i == N-1 and j == N-1:
-        ans += 1
-        return
-    board[i][j] = 3
-
-    if d == 0:
-        if 0<= i <N and 0<= j+1<N:
-            if board[i][j+1] != 1:
-                search(i,j+1,0)
-
-        if 0<= i+1 <N and 0<= j+1<N:
-            if board[i+1][j+1] != 1 and board[i+1][j] != 1 and board[i][j+1] != 1:
-                search(i+1,j+1,2)
-    elif d == 1:
-        if 0 <= i+1 < N and 0 <= j < N:
-            if board[i+1][j] != 1:
-                search(i+1, j, 1)
-
-        if 0 <= i + 1 < N and 0 <= j + 1 < N:
-            if board[i+1][j+1] != 1 and board[i+1][j] != 1 and board[i][j+1] != 1:
-                search(i + 1, j + 1, 2)
-        pass
-    elif d == 2:
-        if 0<= i <N and 0<= j+1<N:
-            if board[i][j + 1] != 1:
-                search(i, j + 1, 0)
-
-        if 0 <= i+1 < N and 0 <= j < N:
-            if board[i+1][j] != 1:
-                search(i + 1, j, 1)
-
-        if 0 <= i + 1 < N and 0 <= j + 1 < N:
-            if board[i+1][j+1] != 1 and board[i+1][j] != 1 and board[i][j+1] != 1:
-                search(i + 1, j + 1, 2)
 
 import sys
 N = int(input())
 board = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
-board[0][0], board[0][1] = 3,3
-ans = 0
-search(0,1,0)
-print(ans)
+dp = [[[0]*N for _ in range(N)] for _ in range(3)]    # dp[0] 가로이동기록, dp[1] 세로이동기록, dp[2] 대각선이동기록
+
+for i in range(N):          # 첫줄은 가로로만 이동이 가능하다.
+    if board[0][i] == 0:
+        dp[0][0][i] = 1
+    else:
+        break
+
+for i in range(1,N):        # 첫줄은 위에서 이미 설정함.
+    for j in range(2,N):    # 최초에 가로로 설정했기때문에 세로0과 세로1은 설정할 수 없음.
+        # 대각선 먼저 체크를 해보겠습니다.
+
+        # 현재위치가 대각선이라면, 대각선-대각선, 가로-대각선, 세로-대각선의 합이다.
+        if board[i][j] == 0 and board[i-1][j] == 0 and board[i][j-1] == 0:
+            dp[2][i][j] = dp[2][i-1][j-1] + dp[1][i-1][j-1] + dp[0][i-1][j-1]
+
+        # 가로 세로를 확인하겠습니다.
+        if board[i][j] == 0:
+            # 현재위치가 세로일경우, 대각선에서 세로로 오는경우 + 세로에서 세로로 오는경우
+            dp[1][i][j] = dp[2][i-1][j] + dp[1][i-1][j]
+            # 현재위치가 가로일경우, 대각선에서 가로로 오는경우 + 가로에서 가로로 오는경우
+            dp[0][i][j] = dp[2][i][j-1] + dp[0][i][j-1]
+
+print(dp[0][N-1][N-1] + dp[1][N-1][N-1] + dp[2][N-1][N-1])

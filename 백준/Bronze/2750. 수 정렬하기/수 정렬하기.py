42562f74556079a8f7360eb6009075f24d000182
song7351 <51012537+song7351@users.noreply.github.com>
2022-10-31 21:45:20 +0900

    [Bronze II] Title: 수 정렬하기, Time: 108 ms, Memory: 30852 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,10 +1,9 @@
-a = int(input())
-
-n = []
-for i in range(a):
-    b = int(input())
-    n.append(b)
-n.sort()
-
-for i in range(a):
-    print(n[i])
+"""
+https://www.acmicpc.net/problem/2750
+오름차순으로 정렬하세요.
+"""
+n = int(input())
+lst = [int(input()) for _ in range(n)]
+lst.sort()
+for x in lst:
+    print(x)

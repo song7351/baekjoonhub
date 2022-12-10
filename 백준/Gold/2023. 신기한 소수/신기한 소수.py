1906e01f8ff6254821d092b33789bb0c1c95dbc1
song7351 <51012537+song7351@users.noreply.github.com>
2022-12-11 02:38:17 +0900

    [Gold V] Title: 신기한 소수, Time: 6952 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -0,0 +1,37 @@
+"""
+N자리수 신기한 소수를 찾아 오름차순으로 출력하세요.
+신기한 소수란?
+왼쪽부터
+1자리도 소수
+2자리도 소수
+3자리도 소수
+...... N자리도 소수
+"""
+import sys
+sys.setrecursionlimit(10**9)
+
+def check(x):
+    for i in range(2,x//2 + 1):
+        if x % i == 0:
+            return False
+    return True
+
+def dfs(x):
+    # 소수인지 확인
+    if check(x):
+        # 길이가 N자리인가?
+        if len(str(x)) == n:
+            print(x)
+        # 길이가 짧다면,
+        elif len(str(x)) < n:
+            str_x = str(x)
+            # 1~9를 붙여라
+            for i in range(1,10):
+                tmp_x = str_x + str(i)
+                dfs(int(tmp_x))
+
+n = int(input())
+
+# 1자리소수는 2,3,5,7
+for i in [2,3,5,7]:
+    dfs(i)

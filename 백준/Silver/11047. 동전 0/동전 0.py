6e7a916f139252771fb4ccdf3c7f137f49e4100a
song7351 <51012537+song7351@users.noreply.github.com>
2022-12-13 16:32:23 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,14 +1,16 @@
+"""
+ 동전은 총 N종류이고,
+  그 가치의 합을 K로 만들려고 한다
+  필요한 동전 개수의 최솟값
+"""
 N, K = map(int, input().split())
+# 돈의 크기가 오름차순으로 제시된다.
+lst = [int(input()) for _ in range(N)]
+ans = 0
 
-lst = []
-for _ in range(N):
-    lst.append(int(input()))
-
-cnt = 0
-
+# 가장 큰 단위부터 나눈다.
 for i in range(N-1, -1, -1):
-    if K // lst[i] != 0:
-        cnt += K//lst[i]
-        K = K % lst[i]
+    ans += K // lst[i]
+    K = K % lst[i]
 
-print(cnt)
+print(ans)

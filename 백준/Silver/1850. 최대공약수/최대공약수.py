92f584a3ff0e6ab2b1dd6f40fcf16e87b357df85
song7351 <51012537+song7351@users.noreply.github.com>
2022-12-28 15:04:22 +0900

    [Silver I] Title: 최대공약수, Time: 2140 ms, Memory: 32812 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.py
@@ -0,0 +1,12 @@
+"""
+a,b 최대 공약수를 구하라
+단 a,b는 1의 개수다
+즉, 3,5가 주어짐ㄴ 111, 11111의 최대 공약수를 구하라
+
+"""
+import math
+
+a,b = map(int, input().split())
+c = math.gcd(a,b)
+for _ in range(c):
+    print('1', end='')

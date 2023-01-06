8934383c3cfa12b1b365e05072cab10fe2602146
song7351 <51012537+song7351@users.noreply.github.com>
2023-01-06 15:18:19 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 226 ms, Memory: 63,048 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -1,38 +1,23 @@
-'''
-모든 경우에 해당하는 합들을 구한다.
-'''
+for _ in range(10):
+    tc = int(input())
+    arr = [ list(map(int, input().split())) for _ in range(100) ]
 
-for tc in range(1,10+1):     # 총 10번의 테스트
-    num_board = []
-    max_ans = 0 
-    right_diagonal = 0
-    left_diagonal = 0
-    N = int(input())    # 테스트케이스 번호
-    
-    for i in range(100):   # 각 행마다 값을 받는다
-        num_list = list(map(int, input().split()))
-        right_diagonal += num_list[i]
-        left_diagonal += num_list[99-i]
-        num_board.append(num_list)
-    
-    for i in num_board:     # 가로행 합
-        sum = 0
-        for j in i:
-            sum += j
-        if sum > max_ans:
-            max_ans = sum
+    ans = []
 
-    for i in range(100):     # 세로행 합
-        sum = 0
+    a,b = 0,0
+    for i in range(100):
+        c,d = 0,0 
         for j in range(100):
-            sum += num_board[j][i]
-        if sum > max_ans:
-            max_ans = sum
-            
-    if right_diagonal > max_ans:
-        max_ans = right_diagonal
-    
-    if left_diagonal > max_ans:
-        max_ans = left_diagonal
+            if i == j:
+                a += arr[i][j]
+            if i + j == 99:
+                b += arr[i][j]
+            c += arr[i][j]
+            d += arr[j][i]
+        
+        ans.append(c)
+        ans.append(d)
+    ans.append(a)
+    ans.append(b)
 
-    print(f"#{tc} {max_ans}")
+    print(f"#{tc} {max(ans)}")

3c154675ef6e47b7d3da9befc7706a4f0ddedeb1
song7351 <51012537+song7351@users.noreply.github.com>
2023-01-29 22:33:59 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 348 ms, Memory: 49048 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,18 +1,26 @@
+"""
+최소신장트리
+여러개의 경로 노드가 제공된다. 우리는 그중 모든 노드가 연결된 그래프중 최소값을 찾는다.
+
+방법.
+1. 가중치가 가장 작은 순서의 연결 노드를 선택
+2. 그 2개의 노드가 아직 연결이 안되어 있다면 연결
+2-1. 연결(x) == 부모노드가 같은가(find)? 다르다면 연결(union)
+"""
 import sys
-from queue import PriorityQueue
+import heapq
 
 input = sys.stdin.readline
-n,m = map(int, input().split())
-pq = PriorityQueue()
-parent = [0] * (n+1)
 
-for i in range(n+1):
-    parent[i] = i
-    
-for i in range(m):
-    s,e,w = map(int, input().split())
-    pq.put((w,s,e))
-    
+n,m = map(int, input().split())
+
+edge = []
+parent = [i for i in range(n+1)]
+
+for _ in range(m):
+    a,b,c = map(int, input().split())
+    heapq.heappush(edge, (c,a,b))
+
 def find(a):
     if a == parent[a]:
         return a
@@ -25,14 +33,13 @@ def union(a,b):
     if a != b:
         parent[b] = a
 
-cnt = 0
-ans = 0
+cnt, ans = 0,0
 
 while cnt < n-1:
-    w,s,e = pq.get()
-    if find(s) != find(e):
-        union(s,e)
-        ans += w
+    c,a,b = heapq.heappop(edge)
+    if find(a) != find(b):
+        union(a,b)
         cnt += 1
-        
+        ans += c
+
 print(ans)

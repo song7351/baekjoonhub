9c65cc5eb98d683ce73ff875e7c007dbef215d70
song7351 <51012537+song7351@users.noreply.github.com>
2023-06-29 15:04:53 +0900

    [unrated] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
@@ -1,16 +1,11 @@
-"""
-s<= <e
-"""
-from collections import deque
-
 def solution(targets):
     answer = 0
-    targets.sort(key=lambda x : x[1])
+    targets.sort(key=lambda x : x[1])   # 요격의 끝이 가장 빠른 순
     missile = -1
     for taget in targets:
         s,e = taget
-        if s > missile:
+        if s > missile:                 # 시작점이 미사일지점보다 크다면, 미사일 추가발사해야됨
             answer += 1
-            missile = e - 0.5
+            missile = e - 0.5           # 편의상 미사일지점은 끝점에서 -0.5
             
     return answer

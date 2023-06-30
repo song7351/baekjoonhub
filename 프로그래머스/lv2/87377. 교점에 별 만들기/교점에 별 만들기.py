f5732e218f78278e88d7051f65f25e927c30cf3f
song7351 <51012537+song7351@users.noreply.github.com>
2023-06-30 13:15:24 +0900

    [level 2] Title: 교점에 별 만들기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.py
@@ -0,0 +1,37 @@
+def solution(line):
+    answer = []
+    n = len(line)
+    lst = set()
+    INF = int(1e15)
+    left, right, bottom, top = INF, -INF, INF, -INF
+    for i in range(n - 1):
+        a, b, e = line[i]
+        for j in range(i + 1, n):
+            c, d, f = line[j]
+            adbc = a * d - b * c
+            bfed_x = b * f - e * d
+            bfed_y = e * c - a * f
+            if adbc != 0:
+                x = bfed_x / adbc
+                y = bfed_y / adbc
+                if x == int(x) and y == int(y):
+                    if x < left:
+                        left = int(x)
+                    if x > right:
+                        right = int(x)
+                    if y < bottom:
+                        bottom = int(y)
+                    if y > top:
+                        top = int(y)
+                    lst.add((int(x), int(y)))
+    n, m = top - bottom + 1, right - left + 1
+    maps = [["."] * m for _ in range(n)]
+    for x, y in lst:
+        xx = x + (-left)
+        yy = -(y + (-top))
+        maps[yy][xx] = '*'
+
+    for lst1 in maps:
+        answer.append(''.join(lst1))
+
+    return answer
